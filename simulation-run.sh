4b70832bd3fa47c0aff9ab380aa9a97da7ec30b5
Guillaume Corré <gui.corre@gmail.com>
2024-03-05 17:53:20 +0100

    fix: update simulation run script with new parameter names

--- simulation-run.sh
+++ simulation-run.sh
@@ -9,8 +9,8 @@ root_dir="$(dirname "$(realpath -- "$0")")"
 
 #"$GRAALVM_HOME/bin/java" -jar "$root_dir/jvm-adapter/target/scala-2.13/gatling-jvm-to-js-adapter-assembly-1.0.0-SNAPSHOT.jar" "$root_dir/js-simulation/target/index.js"
 
-cd "$root_dir/jvm-adapter"
+cd "$root_dir/jvm"
 sbt -java-home "$GRAALVM_HOME" \
-  "-Dgatling.js.bundlePath=$root_dir/js-simulation/target/bundle.js" \
+  "-Dgatling.js.bundle.filePath=$root_dir/js-simulation/target/bundle.js" \
   "-Dgatling.js.entryPoint=mySimulation" \
-  "runMain io.gatling.app.Gatling --simulation io.gatling.js.JsSimulation"
+  "gatling-jvm-to-js-adapter/Compile/runMain io.gatling.app.Gatling --simulation io.gatling.js.JsSimulation"
