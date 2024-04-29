bebd42d4ca57d97c6418704f2ca97fe3fdc0fc5b
Guillaume Corré <gui.corre@gmail.com>
2024-04-29 18:12:39 +0200

    feat: add missing methods in Session

--- js/core/src/session.ts
+++ js/core/src/session.ts
@@ -7,12 +7,34 @@ import JvmSession = io.gatling.javaapi.core.Session;
 export interface Session extends Wrapper<JvmSession> {
   get<T>(key: string): T;
   set(key: string, value: any): Session;
+  setAll(newAttributes: Record<string, any>): Session;
+  remove(key: string): Session;
+  reset(): Session;
+  removeAll(...keys: string[]): Session;
+  contains(key: string): boolean;
+  isFailed(): boolean;
+  markAsSucceeded(): Session;
+  markAsFailed(): Session;
+  scenario(): string;
+  groups(): string[];
+  userId(): number;
 }
 
 export const wrapSession = (_underlying: JvmSession): Session => ({
   _underlying,
   get: <T>(key: string): T => _underlying.get(key),
-  set: (key: string, value: any): Session => wrapSession(_underlying.set(key, value))
+  set: (key: string, value: any): Session => wrapSession(_underlying.set(key, value)),
+  setAll: (newAttributes: Record<string, any>): Session => wrapSession(_underlying.setAll(newAttributes as any)),
+  remove: (key: string): Session => wrapSession(_underlying.remove(key)),
+  reset: (): Session => wrapSession(_underlying.reset()),
+  removeAll: (...keys: string[]): Session => wrapSession(_underlying.removeAll(...keys)),
+  contains: (key: string): boolean => _underlying.contains(key),
+  isFailed: (): boolean => _underlying.isFailed(),
+  markAsSucceeded: (): Session => wrapSession(_underlying.markAsSucceeded()),
+  markAsFailed: (): Session => wrapSession(_underlying.markAsFailed()),
+  scenario: (): string => _underlying.scenario(),
+  groups: (): string[] => _underlying.groups(),
+  userId: (): number => _underlying.userId()
 });
 
 export type Expression<T> = T | ((session: Session) => T);
