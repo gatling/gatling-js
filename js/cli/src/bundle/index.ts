5f598463c56c8f2f043b6594acb83bf741cf02cf
Guillaume Galy <ggaly@gatling.io>
2024-10-21 09:05:33 +0200

    feat: Add minimal polyfills for node.js modules

--- js/cli/src/bundle/index.ts
+++ js/cli/src/bundle/index.ts
@@ -1,8 +1,9 @@
 import * as esbuild from "esbuild";
 import esbuildPluginTsc from "esbuild-plugin-tsc";
 
-import { SimulationFile } from "./simulations";
-import { logger } from "./log";
+import { polyfill } from "./polyfill";
+import { SimulationFile } from "../simulations";
+import { logger } from "../log";
 
 export interface BundleOptions {
   sourcesFolder: string;
@@ -20,12 +21,15 @@ export const bundle = async (options: BundleOptions): Promise<void> => {
   const contents = options.simulations.map((s) => `export { default as "${s.name}" } from "./${s.path}";`).join("\n");
 
   const plugins = options.typescript ? [esbuildPluginTsc({ force: true })] : [];
+  plugins.push(polyfill());
   await esbuild.build({
     stdin: {
       contents,
       resolveDir: options.sourcesFolder
     },
     outfile: options.bundleFile,
+    platform: "neutral",
+    mainFields: ["main", "module"],
     bundle: true,
     minify: false,
     sourcemap: true,
