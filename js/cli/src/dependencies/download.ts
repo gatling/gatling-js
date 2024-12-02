450835d402748dbb759b69bcad6b38faf7eda15b
Guillaume Corré <gui.corre@gmail.com>
2024-12-02 17:27:02 +0100

    feat: read proxy settings from npmrc files

--- js/cli/src/dependencies/download.ts
+++ js/cli/src/dependencies/download.ts
@@ -1,11 +1,32 @@
-import axios from "axios";
+import fetch, { MakeFetchHappenOptions, TlsOptions } from "make-fetch-happen";
 import fs from "fs";
-import stream from "stream";
 import util from "util";
+import stream from "stream";
 
 const pipeline = util.promisify(stream.pipeline);
 
+const NPM_CONFIG_PROXY_KEY = "npm_config_proxy";
+const NPM_CONFIG_HTTPS_PROXY_KEY = "npm_config_https_proxy";
+const NPM_CONFIG_NOPROXY_KEY = "npm_config_noproxy";
+
+const fetchOptionsFromNpmConfiguration = (): MakeFetchHappenOptions & TlsOptions => {
+  const proxy = process.env[NPM_CONFIG_HTTPS_PROXY_KEY] || process.env[NPM_CONFIG_PROXY_KEY] || undefined;
+  let noProxy = process.env[NPM_CONFIG_NOPROXY_KEY] || undefined;
+  if (noProxy === "") {
+    noProxy = undefined;
+  }
+  return {
+    proxy,
+    noProxy
+  };
+};
+
 export const downloadFile = async (url: string, targetFile: string): Promise<void> => {
-  const request = await axios.get(url, { responseType: "stream" });
-  await pipeline(request.data, fs.createWriteStream(targetFile));
+  const options = fetchOptionsFromNpmConfiguration();
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+  }
+
+  await pipeline(response.body, fs.createWriteStream(targetFile));
 };
