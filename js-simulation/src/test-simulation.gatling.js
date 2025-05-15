1581df772f7db75784a1c52197839d2259b4b4a0
Guillaume Corré <gui.corre@gmail.com>
2025-05-15 16:51:30 +0200

    chore: use new ecommerce demo site for samples

--- js-simulation/src/test-simulation.gatling.js
+++ js-simulation/src/test-simulation.gatling.js
@@ -1,82 +1,31 @@
-import {
-  constantUsersPerSec,
-  scenario,
-  simulation,
-  exec,
-  csv,
-  jsonFile,
-  group,
-  pause,
-  arrayFeeder,
-  atOnceUsers,
-  nothingFor,
-  doWhile,
-  css,
-  global
-} from "@gatling.io/core";
+import { simulation, atOnceUsers, global, scenario, getParameter } from "@gatling.io/core";
 import { http } from "@gatling.io/http";
 
-export default simulation(setUp => {
-  const baseHttpProtocol = http.baseUrl("https://computer-database.gatling.io");
+export default simulation((setUp) => {
+  // Load VU count from system properties
+  // Reference: https://docs.gatling.io/guides/passing-parameters/
+  const vu = parseInt(getParameter("vu", "1"));
 
-  const searchFeeder = arrayFeeder([
-    { searchCriterion: "Macbook", searchComputerName: "MacBook Pro" },
-    { searchCriterion: "eee", searchComputerName: "ASUS Eee PC 1005PE" }
-  ]).random();
-
-  // Either CSV or JSON feeder can be used in this example:
-  // const createFeeder = csv("computers/create.csv").circular();
-  const createFeeder = jsonFile("computers/create.json").circular();
-
-  const scn = scenario("My scenario")
-    .feed(searchFeeder)
-    .feed(createFeeder)
-    .exec(
-      group("Browse").on(
-        exec(session => session.set("page", 1)),
-        doWhile(session => session.get("page") < 3).on(
-          http("Browse page #{page}").get("/computers?p=#{page}"),
-          exec(session => session.set("page", session.get("page") + 1)),
-          pause({ amount: 500, unit: "milliseconds" })
-        )
-      ),
-      group("Search").on(
-        http("Search").get("/computers?f=#{searchCriterion}")
-        .check(
-          css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
-        ),
-        http("Select").get("#{computerUrl}")
-      ),
-      group("Create").on(
-        http("Get form").get("/computers/new"),
-        pause(1),
-        exec(session => {
-          const name = session.get("name");
-          const introduced = session.get("introduced");
-          const discontinued = session.get("discontinued");
-          const company = session.get("company");
-          console.log(
-            `Create computer (name='${name}', introduced='${introduced}', discontinued='${discontinued}', company='${company}')`
-          );
-          return session;
-        }),
-        http("Post form")
-          .post("/computers")
-          .formParam("name", "Beautiful Computer")
-          .formParam("introduced", "2012-05-30")
-          .formParam("discontinued", "")
-          .formParam("company", "37")
-      )
+  // Define HTTP configuration
+  // Reference: https://docs.gatling.io/reference/script/protocols/http/protocol/
+  const httpProtocol = http
+    .baseUrl("https://api-ecomm.gatling.io")
+    .acceptHeader("application/json")
+    .userAgentHeader(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
     );
 
-  setUp(
-    scn.injectOpen(
-      atOnceUsers(10),
-      nothingFor({ amount: 5, unit: "seconds" }),
-      constantUsersPerSec(2).during(30)
-    )
-  ).protocols(baseHttpProtocol)
-    .assertions(
-      global().responseTime().percentile(95.0).lte(500)
-    );
+  // Define scenario
+  // Reference: https://docs.gatling.io/reference/script/core/scenario/
+  const scn = scenario("Scenario").exec(http("Session").get("/session"));
+
+  // Define assertions
+  // Reference: https://docs.gatling.io/reference/script/core/assertions/
+  const assertion = global().failedRequests().count().lt(1.0);
+
+  // Define injection profile and execute the test
+  // Reference: https://docs.gatling.io/reference/script/core/injection/
+  setUp(scn.injectOpen(atOnceUsers(vu)))
+    .assertions(assertion)
+    .protocols(httpProtocol);
 });
--- ts-simulation/src/test-simulation.gatling.ts
+++ ts-simulation/src/test-simulation.gatling.ts
@@ -1,98 +1,32 @@
-import {
-  constantUsersPerSec,
-  scenario,
-  simulation,
-  Session,
-  ScenarioBuilder,
-  exec,
-  csv,
-  jsonFile,
-  group,
-  pause,
-  arrayFeeder,
-  atOnceUsers,
-  nothingFor,
-  doWhile,
-  css,
-  global,
-  GlobalStore, readResourceAsString, readResourceAsBytes
-} from "@gatling.io/core";
+import { simulation, scenario, atOnceUsers, global, getParameter } from "@gatling.io/core";
 import { http } from "@gatling.io/http";
 
 export default simulation((setUp) => {
-  const baseHttpProtocol = http.baseUrl("https://computer-database.gatling.io");
+  // Load VU count from system properties
+  // Reference: https://docs.gatling.io/guides/passing-parameters/
+  const vu = parseInt(getParameter("vu", "1"));
 
-  const searchFeeder = arrayFeeder([
-    { searchCriterion: "Macbook", searchComputerName: "MacBook Pro" },
-    { searchCriterion: "eee", searchComputerName: "ASUS Eee PC 1005PE" }
-  ]).random();
+  // Define HTTP configuration
+  // Reference: https://docs.gatling.io/reference/script/protocols/http/protocol/
+  const httpProtocol = http
+    .baseUrl("https://api-ecomm.gatling.io")
+    .acceptHeader("application/json")
+    .userAgentHeader(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
 
-  // Either CSV or JSON feeder can be used in this example:
-  // const createFeeder = csv("computers/create.csv").circular();
-  const createFeeder = jsonFile("computers/create.json").circular();
-
-  console.log(`JSON file: ${String.fromCharCode(...readResourceAsBytes("computers/create.csv"))}`);
-
-  const scn: ScenarioBuilder = scenario("My scenario")
-    .feed(searchFeeder)
-    .feed(createFeeder)
-    .exec(
-      group("Browse").on(
-        exec(session => session.set("page", 1)),
-        doWhile((session: Session) => session.get<number>("page") <= 3).on(
-          http("Browse page #{page}").get("/computers?p=#{page}"),
-          exec(session => {
-            GlobalStore.update("browsedPages", old => typeof old === "number" ? old + 1 : 1);
-            return session.set("page", session.get<number>("page") + 1)
-          }),
-          pause({ amount: 500, unit: "milliseconds" })
-        )
-      ),
-      group("Search").on(
-        http("Search").get("/computers?f=#{searchCriterion}")
-        .check(
-          css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
-        ),
-        http("Select").get("#{computerUrl}")
-      ),
-      group("Create").on(
-        http("Get form").get("/computers/new"),
-        pause(1),
-        exec(session => {
-          const name = session.get<string>("name");
-          const introduced = session.get<string>("introduced");
-          const discontinued = session.get<string>("discontinued");
-          const company = session.get<string>("company");
-          console.log(
-            `Create computer (name='${name}', introduced='${introduced}', discontinued='${discontinued}', company='${company}')`
-          );
-          return session;
-        }),
-        http("Post form")
-          .post("/computers")
-          .formParam("name", "Beautiful Computer")
-          .formParam("introduced", "2012-05-30")
-          .formParam("discontinued", "")
-          .formParam("company", "37")
-      )
     );
 
-  setUp(
-    scn.injectOpen(
-      atOnceUsers(1)
-      // atOnceUsers(10),
-      // nothingFor({ amount: 5, unit: "seconds" }),
-      // constantUsersPerSec(2).during(30)
-    ).andThen(
-      scenario("Post execution")
-        .exec(session => {
-          console.log(`browsedPages=${GlobalStore.get("browsedPages")}`);
-          return session;
-        })
-        .injectOpen(atOnceUsers(1))
-    )
-  ).protocols(baseHttpProtocol)
-    .assertions(
-      global().responseTime().percentile(95.0).lte(500)
-    );
+  // Define scenario
+  // Reference: https://docs.gatling.io/reference/script/core/scenario/
+  const scn = scenario("Scenario").exec(http("Session").get("/session"));
+
+  // Define assertions
+  // Reference: https://docs.gatling.io/reference/script/core/assertions/
+  const assertion = global().failedRequests().count().lt(1.0);
+
+  // Define injection profile and execute the test
+  // Reference: https://docs.gatling.io/reference/script/core/injection/
+  setUp(scn.injectOpen(atOnceUsers(vu)))
+    .assertions(assertion)
+    .protocols(httpProtocol);
 });
