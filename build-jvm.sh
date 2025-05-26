8edaca3c47a455cef806f18d12fd3ce84ed9ed2c
Guillaume Corré <gui.corre@gmail.com>
2025-05-26 11:48:07 +0200

    feat: add ability to override local bundle jars when publishing the jvm adapter

--- build-jvm.sh
+++ build-jvm.sh
@@ -7,3 +7,12 @@ root_dir="$(dirname "$(realpath -- "$0")")"
 # Publish JVM adapter
 cd "$root_dir/jvm"
 sbt gatling-jvm-to-js-adapter/publishLocal
+
+if [[ "$1" = "--bundle" ]]; then
+  publish_version=0.0.0-SNAPSHOT
+  publish_path=$HOME/.ivy2/local/io.gatling/gatling-jvm-to-js-adapter/$publish_version/jars
+  bundle_path=$HOME/.gatling/gatling-js-bundle/$publish_version/lib/java
+
+  cp "$publish_path/gatling-jvm-to-js-adapter.jar" \
+    "$bundle_path/gatling-jvm-to-js-adapter.jar"
+fi
